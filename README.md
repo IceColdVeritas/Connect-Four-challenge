939df8d3632501a3e77669fb459ccc8e13b0ca7a
Fidel <fidel@Fidels-MacBook-Pro.local>
2023-08-12 18:39:50 +0200

    Game working. Upload to BQ not solved - writing errors

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # Connect-Four-challenge
 Determine the winner in a Connect Four game
+
+## Create a virtual environment in your terminal
+Before starting installing packages, it's best to create an isolated virtual environment.
+One simple option is to use Python's build in native tool.
+
+To create a virtual environment, execute the following code in your terminal:
+```javascript Python
+python3 -m venv <name_of_virtualenv>
+```
+
+To activate it, execute:
+```javascript Python
+source <name_of_virtualenv>/bin/activate
+```
+
+When you're done, you can deactivate your virtual environment with:
+``` javascript Python
+deactivate
+```
+## Install the packages in the requirements file
+Execute the following command to install all dependencies needed, which are listed in the requirements.txt file:
+``` javascript Python
+pip install -r requirements.txt
+```
+## Set up environmental variables
+Set up your dataset id and table id as environmental variables to connect to an external database (BigQuery here) to make it more secure by not leaving any trace of it in your IDE.
+
+To set up said variables, execute the following code snippets:
+```bash Python
+export CONNECT4_DATASET_ID=your_project_id
+export CONNECT4_DATASET_ID=your_dataset_id
+export CONNECT4_TABLE_ID=your_table_id
+```
+
+Without it your game results data will not be populated in your table in your bigquery project!
--- main_app.py
+++ main_app.py
@@ -1,115 +1,203 @@
 from itertools import groupby
+import numpy as np
+from google.cloud import bigquery
 
-# function that takes in a list of players and a list of moves and calculates the winner in a game of Connect Four
-# def calculate_winner(players, moves):
-#     # create a list of lists to represent the game board
-#     board = [[' ' for x in range(7)] for y in range(6)]
-
-#     # loop through the moves and place the pieces on the board
-#     for move in moves:
-#         # split the move into a list
-#         move = move.split(',')
-#         # place the piece on the board
-#         board[int(move[1])][int(move[0])] = move[2]
-
-#     # loop through the board and check for a horizontal win
-#     for row in board:
-#         # loop through the row
-#         for i in range(len(row) - 3):
-#             # check if there are four pieces in a row
-#             if row[i] == row[i + 1] == row[i + 2] == row[i + 3] and row[i] != ' ':
-#                 # return the winner
-#                 return row[i]
-
-#     # loop through the board and check for a vertical win
-#     for i in range(len(board) - 3):
-#         # loop through the column
-#         for j in range(len(board[i])):
-#             # check if there are four pieces in a column
-#             if board[i][j] == board[i + 1][j] == board[i + 2][j] == board[i + 3][j] and board[i][j] != ' ':
-#                 # return the winner
-#                 return board[i][j]
-
-#     # loop through the board and check for a diagonal win
-#     for i in range(len(board) - 3):
-#         # loop through the row
-#         for j in range(len(board[i]) - 3):
-#             # check if there are four pieces in a diagonal
-#             if board[i][j] == board[i + 1][j + 1] == board[i + 2][j + 2] == board[i + 3][j + 3] and board[i][j] != ' ':
-#                 # return the winner
-#                 return board[i][j]
-
-#     # loop through the board and check for a diagonal win
-#     for i in range(len(board) - 3):
-#         # loop through the row
-#         for j in range(len(board[i]) - 3):
-#             # check if there are four pieces in a diagonal
-#             if board[i][j + 3] == board[i + 1][j + 1 + 2] == board[i + 2][j + 2 + 1] == board[i + 3][j + 3] and board[i][j] != ' ':
-#                 # return the winner
-#                 return board[i][j + 3]
+import os
+import logging
 
 
-def check_winner(board, player):
-    # Check for vertical wins
-    for col in range(7):
-        for row in range(3):
-            if all(board[row+i][col] == player for i in range(4)):
-                return True
-    
-    # Check for horizontal wins
-    for row in range(6):
-        for col in range(4):
-            if all(board[row][col+i] == player for i in range(4)):
-                return True
-    
-    # Check for diagonal wins (both directions)
-    for row in range(3):
-        for col in range(4):
-            if all(board[row+i][col+i] == player for i in range(4)):
-                return True
-            if all(board[row+i][col+3-i] == player for i in range(4)):
-                return True
-    
-    return False
-
+ROW_COUNT = 6
+COLUMN_COUNT = 7
 
 # read the file
 with open("/Users/fidel/Downloads/connect_four/matchdata.txt", "r") as f:
     lines = f.readlines()
-    players = []
-    moves = []
+    player_groups = []
+    games = []
 
     # read the file and split the data into two lists
     lists = [list(g) for k, g in groupby(lines, lambda x: x == '\n') if not k]
 
     # remove the \n from the lists and append the names and moves into two lists
     for list in lists:
-        players.append(list[0].strip('\n').split(','))
-        moves.append(list[1].strip('\n').split(','))
+        player_groups.append(list[0].strip('\n').split(','))
+        games.append(list[1].strip('\n').split(','))
 
-    print(players)
-    print(moves)
 
-# Initialize an empty game board
-board = [['' for _ in range(7)] for _ in range(6)]
+# Create an empty game board
+def create_board():
+    board = np.zeros((ROW_COUNT, COLUMN_COUNT), dtype=str)
+    return board
 
-# Convert moves and update the board
-for move in moves:
-    for i, m in enumerate(move):
+
+def drop_piece(board, row, col, piece):
+    board[row][col] = piece
+
+
+def get_next_open_row(board, col):
+    for r in range(ROW_COUNT-1, -1, -1):
+        if board[r][col] == '':
+            return r
+
+
+def winning_move(board, piece):
+    # Check horizontal locations for win
+    for c in range(COLUMN_COUNT-3):
+        for r in range(ROW_COUNT):
+            if all(board[r][c+i] == piece for i in range(4)):
+                return True
+
+    # Check vertical locations for win
+    for c in range(COLUMN_COUNT):
+        for r in range(ROW_COUNT-3):
+            if all(board[r+i][c] == piece for i in range(4)):
+                return True
+
+    # Check positively sloped diagonals
+    for c in range(COLUMN_COUNT-3):
+        for r in range(ROW_COUNT-3):
+            if all(board[r+i][c+i] == piece for i in range(4)):
+                return True
+
+    # Check negatively sloped diagonals
+    for c in range(COLUMN_COUNT-3):
+        for r in range(3, ROW_COUNT):
+            if all(board[r-i][c+i] == piece for i in range(4)):
+                return True
+
+game_results = []
+
+for game, players in zip(games, player_groups):
+    # Initialize an empty game board after each game
+    board = create_board()
+    print()
+    print("Board created")
+    print("Game between " + players[0] + " and " + players[1] + " started!")
+
+    for i, moves in enumerate(game):
         player = players[i % 2]
-        col = int(m[1])
-        for row in range(5, -1, -1):
-            if board[row][col] == '':
-                board[row][col] = player
-                break
-
-        if check_winner(board, player):
+        col = int(moves[1])
+        col -= 1
+        row = get_next_open_row(board, col)
+        drop_piece(board, row, col, moves[0])
+        
+        if winning_move(board, moves[0]):
+            # Prepare the game results data
+            game_results.append({"player1": players[0], "player2": players[1], "winner": player})
             print(f"{player} wins!")
             break
 
-# If no winner is found, it's a draw
-else:
-    print("It's a draw!")
-# if __name__=='__main__':
-#     calculate_winner(players, moves)
+    else:
+        print("It's a draw!")
 
+    print(board)
+
+print(game_results)
+
+# Initialize a BigQuery client
+client = bigquery.Client()
+
+# Define project, dataset and table variables
+project_id = os.environ.get('CONNECT4_PROJECT_ID')
+dataset_id = os.environ.get('CONNECT4_DATASET_ID')
+table_id = os.environ.get('CONNECT4_TABLE_ID')
+
+# Define the dataset reference
+dataset_ref = client.dataset(dataset_id, project=project_id)
+logging.info(f"dataset_ref: {dataset_ref}")
+
+# Create the dataset if it doesn't exist
+try:
+    client.get_dataset(dataset_ref)
+except Exception as e:
+    dataset = bigquery.Dataset(dataset_ref)
+    dataset = client.create_dataset(dataset)
+    logging.info(f"Dataset {dataset_id} created.")
+
+# Define the table reference
+table_ref = dataset_ref.table(table_id)
+logging.info(f"table_ref: {table_ref}")
+
+# Job configuration
+schema = [
+    bigquery.SchemaField("player_1", "STRING"),
+    bigquery.SchemaField("player_2", "STRING"),
+    bigquery.SchemaField("winner", "STRING"),
+]
+logging.info(f"Schema is {schema}")
+
+# Define job configuration
+job_config = bigquery.LoadJobConfig(
+    schema=schema,
+    source_format=bigquery.SourceFormat.NEWLINE_DELIMITED_JSON,
+)
+logging.debug("Job config: {}".format(job_config))
+
+# Load data into the table
+job = client.load_table_from_json(game_results, table_ref, job_config=job_config)
+logging.info(f"Job: {job}")
+job.result()  # Wait for the job to complete
+
+print("Data successfully loaded.")
+
+
+
+
+# def check_winner(board, player):
+#     # Check for vertical wins
+#     for col in range(7):
+#         for row in range(3):
+#             if all(board[row+i][col] == player for i in range(4)):
+#                 print('Found a vertical win')
+#                 return True
+    
+#     # Check for horizontal wins
+#     for row in range(6):
+#         for col in range(4):
+#             if all(board[row][col+i] == player for i in range(4)):
+#                 print('Found a horizontal win')
+#                 return True
+    
+#     # Check for diagonal wins (both directions)
+#     for row in range(3):
+#         for col in range(4):
+#             if all(board[row+i][col+i] == player for i in range(4)):
+#                 print('Found a diagonal win')
+#                 return True
+#             if all(board[row+i][col+3-i] == player for i in range(4)):
+#                 print('Found a diagonal win')
+#                 return True
+    
+#     return False
+
+
+# # Convert moves and update the board
+# def play_game(games, player_groups):
+#     for game, players in zip(games, player_groups):
+#         for i, moves in enumerate(game):
+#             player = players[i % 2]
+#             col = int(moves[1])
+#             col -= 1
+#             for row in range(5, -1, -1):
+#                 if board[row][col] == '':
+#                     board[row][col] = moves[0]
+#                     if check_winner(board, player):
+#                         print(f"{player} wins!")
+#                         break
+#                     break
+
+#             # If no winner is found, it's a draw
+#             else:
+#                 print("It's a draw!")
+
+#             # Print the board
+#             for row in board:
+#                 print(row)
+#             print()
+
+#             # Add a log statement
+#             print(f"Player {player} played in column {col+1}\n")
+
+
+# if __name__=='__main__':
+#     play_game(games, player_groups)
